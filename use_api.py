67ee455d957e9af2149fd8121e5eb8bc5b975cf6
Mix510 <18666119673@163.com>
2025-03-20 19:46:07 +0800

    2

--- use_api.py
+++ use_api.py
@@ -1,3 +1,5 @@
+
+
 import requests
 from openai import OpenAI
 
@@ -15,9 +17,15 @@ def get_openai(words):
         ],
         stream=False
     )
-    return response
+    reasoning_content = response.choices[0].message.reasoning_content
+    content = response.choices[0].message.content
+    total_tokens = response.usage.total_tokens
+    return reasoning_content, content, total_tokens
+
+def get_ollama(word):
+    url = 'https://localhost:11434/chat'
 
 
 if __name__ == '__main__':
-    res = get_openai('你好')
-    print(res)
+    rea, con, tok = get_openai('你好')
+    print(rea, con, tok)
